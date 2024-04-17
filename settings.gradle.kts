5b65bbf1a666ae97158ee9b137eefb7fafb8f38e
szainbzu <szain@birzeit.edu>
2024-04-17 16:03:35 +0200

    version1

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "rest_lib_php"
+include(":app")
+ 
